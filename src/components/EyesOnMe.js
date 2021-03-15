0e51cb04af9d05ce6e3838c65835c02487e09103
gracenak <gracenak@gmail.com>
2021-03-14 19:14:45 -0700

    passed test solutions for lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React, {Component} from 'react'
+export default class EyesOnMe extends Component {
+    handleFocusListener = () => {console.log('Good!')}
+    handleButtonListener = () => {console.log('Hey! Eyes on me!')}
+    
+    render(){
+        return(
+            <button 
+                onFocus={this.handleFocusListener} 
+                onBlur={this.handleButtonListener}>
+            </button>
+            
+        )
+    }
+}
